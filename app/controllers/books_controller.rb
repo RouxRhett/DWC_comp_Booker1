0e8e265533b46d4c99fae67e5d3d250c117542a9
RouxRhett <nsch.4871@gmail.com>
2021-11-24 12:34:55 +0900

    [add]編集機能、削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,16 +13,29 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book)
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Book.create!(
-  [
-    {
-      title: 'リーダブルコード',
-      body: 'より良いコードを書くためのシンプルで実践的なテクニック'
-    },
-    {
-      title: 'トラブル知らずのシステム設計',
-      body: 'システム設計の要点を図解で説明してくれます'
-    },
-    {
-      title: 'たのしいRuby',
-      body: 'Rubyの入門におすすめです'
-    },
-    {
-      title: '入門Git',
-      body: 'gitについての基本操作から内部構造まで解説してくれます'
-    },
-    {
-      title: 'アルゴリズム図鑑',
-      body: 'アルゴリズムについてカラーイラストでしっかり解説してくれます'
-    },
-    {
-      title: '達人プログラマー',
-      body: 'プログラマ入門者は必読！'
-    },
-  ]
+Book.create(   
+  title: 'リーダブルコード',
+  body: 'より良いコードを書くためのシンプルで実践的なテクニック'
+)
+
+Book.create( 
+  title: 'トラブル知らずのシステム設計',
+  body: 'システム設計の要点を図解で説明してくれます'
+)
+
+Book.create( 
+  title: 'たのしいRuby',
+  body: 'Rubyの入門におすすめです'
+)
+
+Book.create( 
+  title: '入門Git',
+  body: 'gitについての基本操作から内部構造まで解説してくれます'
+)
+
+Book.create( 
+  title: 'アルゴリズム図鑑',
+  body: 'アルゴリズムについてカラーイラストでしっかり解説してくれます'
+)
+
+Book.create( 
+  title: '達人プログラマー',
+  body: 'プログラマ入門者は必読！'
+)
+
+Book.create( 
+  title: 'データベース実践入門',
+  body: '効率的なSQL文を教えてくれます'
 )
