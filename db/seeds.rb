429ba5363b24f1314297ce156e7ee2d18f185e49
RouxRhett <nsch.4871@gmail.com>
2021-11-24 10:51:00 +0900

    [add][change]全体のレイアウト変更、投稿機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.create!(
+  [
+    {
+      title: 'リーダブルコード',
+      body: 'より良いコードを書くためのシンプルで実践的なテクニック'
+    },
+    {
+      title: 'トラブル知らずのシステム設計',
+      body: 'システム設計の要点を図解で説明してくれます'
+    },
+    {
+      title: 'たのしいRuby',
+      body: 'Rubyの入門におすすめです'
+    },
+    {
+      title: '入門Git',
+      body: 'gitについての基本操作から内部構造まで解説してくれます'
+    },
+    {
+      title: 'アルゴリズム図鑑',
+      body: 'アルゴリズムについてカラーイラストでしっかり解説してくれます'
+    },
+    {
+      title: '達人プログラマー',
+      body: 'プログラマ入門者は必読！'
+    },
+  ]
+)
