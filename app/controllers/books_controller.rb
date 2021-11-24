b1924f77946c898eba712d9c8043bcead4f4b58b
RouxRhett <nsch.4871@gmail.com>
2021-11-24 14:04:57 +0900

    [change]バリデーションの追加、アラートの追加、レイアウトの変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,29 +8,36 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    #@book = Book.new
-  end
+#  def new
+#    @book = Book.new
+#  end
 
   def edit
     @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render action:'index'
+    end
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
     redirect_to book_path(book)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 
