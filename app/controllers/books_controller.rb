ce3f41a1fbee16500fde7dd5cdc946f1e613b113
RouxRhett <nsch.4871@gmail.com>
2021-11-24 14:15:40 +0900

    [change]編集画面でもエラーメッセージが表示される様に変更,エラー部分の記述を変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,10 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = 'Book was successfully updated.'
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book)
+    else
+      render action:'edit'
+    end
   end
 
   def destroy
